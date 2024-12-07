abbf68c3d64359fe190520c8982ab6350db39cd1
Usman Ali <Usman.tpr@gmail.com>
2024-12-07 23:28:34 +0500

    added

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-// src/server.js
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors")
+const cors = require("cors");
+
 dotenv.config();
 
 const connectDB = require("./src/config/db");
 
 const app = express();
-app.use(cors())
+
+// CORS configuration
+const corsOptions = {
+  origin: [
+    "https://afghan.ayancurtains.com",  // Allow requests from this domain
+    "https://admin.afghan.ayancurtains.com"  // Allow requests from this subdomain
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"], // You can customize allowed methods
+  credentials: true,  // Enable sending credentials (cookies, authorization headers, etc.)
+};
+
+// Use CORS middleware with options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-
 
 connectDB(); 
 
 // Routes
-const admin = require("./src/routes/adminRoutes")
-const visit = require("./src/routes/visitRoutes")
-const Event = require("./src/routes/eventRoutes")
+const admin = require("./src/routes/adminRoutes");
+const visit = require("./src/routes/visitRoutes");
+const Event = require("./src/routes/eventRoutes");
 
-
-app.use("/api/user" , admin)
-app.use("/api/visit" , visit)
-app.use("/api/event" , Event)
+app.use("/api/user", admin);
+app.use("/api/visit", visit);
+app.use("/api/event", Event);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
@@ -30,5 +41,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
